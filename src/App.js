d86937ceadcc2c34cf5ed5ccba0478e0fb500400
youngkwonkim <rladudrnjs12@gmail.com>
2021-01-01 23:25:32 +0900

    data transfer with props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
-import Potato from "./Potato.js";
+
+// function Food(props) {
+  function Food({fav}) { // {fav} props라는 object에서 fav를 꺼낸다.
+  /** props를 사용한 데이터 넘기기/받기 */
+  return (
+    <h1>I like {fav}</h1> 
+  )
+}
 
 function App() {
-  const aa = 'aa'
+  const jay = 'jay'
   return (
     <div className="App">
-      <h1>Hello !!! { aa }</h1>
-      <Potato />
+      <h1>Hello !!! { jay }</h1>
+        <Food fav="kimchi" />
+        <Food fav="ramen" />
+        <Food fav="samgopsal" />
     </div>
   )
 }
