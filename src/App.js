474c1b340837b70bf6b7b1dbfbccbc9359ba0c31
youngkwonkim <rladudrnjs12@gmail.com>
2021-01-01 23:01:53 +0900

    component import test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Potato from "./Potato.js";
 
 function App() {
+  const aa = 'aa'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React@
-        </a>
-      </header>
+      <h1>Hello !!! { aa }</h1>
+      <Potato />
     </div>
-  );
+  )
 }
 
 export default App;
